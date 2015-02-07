d9a7a3dcadbfe14049b8bdaec0c1fef01148b968
Colin Regan <khaullen@gmail.com>
2015-02-06 23:45:44 -0800

    Initial Commit

---
+++ AutoCorrupt Custom Keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoCorrupt Custom Keyboard
+//
+//  Created by Colin Regan on 2/6/15.
+//  Copyright (c) 2015 Comedy Hack Day. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
